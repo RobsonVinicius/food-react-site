ec628f3b0c3c6633940d02adc6a8f031f279b4b8
Robson <robsondgn@outlook.com>
2022-01-09 02:51:30 -0300

    Installed dependencies: react-router-dom, material ui, Icons Material

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
 
 function App() {
   return (
     <div className="App">
       Hello World
       <h1>Another Heading </h1>
+      <AcUnitIcon />
     </div>
   );
 }
