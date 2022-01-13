0a05e0bdde7b0d397f21fe5605e9722547a5909a
Robson <robsondgn@outlook.com>
2022-01-13 01:41:08 -0300

    navbar mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from '../assets/logo.png';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
+import ReorderIcon from '@mui/icons-material/Reorder';
 
 function Navbar() {
+  const [showLinks, setShowLInks] = useState(false);
+  const toggleNavbar = () => {
+    setShowLInks(!showLinks);
+  };
   return (
     <div className="navbar">
-      <div className="leftSide">
+      <div className="leftSide" id={showLinks ? "open" : "close"}>
         <img src={logo} />
-      </div>
+        <div className="hiddenLinks">
+          <Link to="/">Home</Link>
+          <Link to="/">Menu</Link>
+          <Link to="/">About</Link>
+          <Link to="/">Contact</Link>
+        </div>
+      </div>      
       <div className="rightSide">
         <Link to="/">Home</Link>
         <Link to="/">Menu</Link>
         <Link to="/">About</Link>
         <Link to="/">Contact</Link>
+        <button onClick={toggleNavbar}>
+          <ReorderIcon/>
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
