7c1d23886d6785007fb63e6a89c363f875590606
Robson <robsondgn@outlook.com>
2022-01-23 18:27:06 -0300

    Adjust import Icons Material

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { Instagram, Twitter, Facebook, LinkedIn } from '@mui/icons-material';
 import '../styles/Footer.css';
 
 function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
-        <InstagramIcon />
-        <TwitterIcon />
-        <FacebookIcon />
-        <LinkedInIcon />
+        <Instagram />
+        <Twitter />
+        <Facebook />
+        <LinkedIn />
       </div>
         <p>&copy; 2022 Brazilian Food</p>
     </div>
